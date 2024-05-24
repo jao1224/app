bcbcb9f3011d86265afc59da9ac74fbf45a2f52c
João Fernando <107001117+jao1224@users.noreply.github.com>
2024-05-24 11:36:19 -0300

    a

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
-from flask_cors import CORS 
+from flask_cors import CORS # type: ignore
 
 app = Flask(__name__)
 CORS(app)
